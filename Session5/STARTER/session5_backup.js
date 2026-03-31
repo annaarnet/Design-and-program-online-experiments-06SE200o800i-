a73029fa7c3afb3dc3e93d4f7f1cd1cccebb312e
annaarnet <annasofia.arnet@uzh.ch>
2026-03-31 09:19:13 +0200

    Add sessions

---
+++ Session5/STARTER/session5_backup.js
@@ -0,0 +1,17 @@
+// let ageGroup; 
+
+// const age = 10; 
+// if (age < 18) {
+// 
+//  ageGroup = "Child"; 
+//} else {ageGroup = "Adult"; 
+
+//}
+// console.log ("ageGroup:", ageGroup);
+
+for (let i = 1; i<=5; i++) {
+    console.log ("i/number:", i);
+}
+
+
+
